b5c32ab092cb09bc07df910dbc9d1d0355078332
Khalid Saifullah <khalideofficial@gmail.com>
2021-03-21 02:10:40 +0600

    mongoose update and bug fixing

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 
 module.exports = function(){
-    mongoose.connect(process.env.CONNECTION_STRING)
+    console.log(process.env.CONNECTION_STRING, process.env.jwtToken)
+    mongoose.connect(process.env.CONNECTION_STRING , 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
         .then(_ => console.log('Connect to mongodb'))
         .catch(_ => console.log('Unable Connect to mongodb'))
 }
