961b5fd1cd89e0d57f2c431393c0e6f32495787c
Khalid Saifullah <khalideofficial@gmail.com>
2021-03-21 02:14:17 +0600

    Cross support

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 require('dotenv').config()
 require('./startup/db')()
 require('./startup/router')(app)
