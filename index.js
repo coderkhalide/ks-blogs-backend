a6b495e838e47f493c371b4ca6128787385d28fd
Khalid Saifullah <khalideofficial@gmail.com>
2021-03-21 04:00:28 +0600

    Cross Issue 2

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-    } else {
-      next();
-    }
-})
+app.use(cors())
 
 require('dotenv').config()
 require('./startup/db')()
