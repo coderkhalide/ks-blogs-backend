d4a065282569f8bbf832999d770bc82682a4822a
Khalid Saifullah <khalideofficial@gmail.com>
2021-03-21 03:48:24 +0600

    Cross Issue

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+    } else {
+      next();
+    }
+})
 
 require('dotenv').config()
 require('./startup/db')()
